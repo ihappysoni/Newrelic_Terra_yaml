19ecb72c0ca6efc5c4b1186ce914b51cd63f2afa
ihappysoni <96763614+ihappysoni@users.noreply.github.com>
2022-11-04 20:32:44 +0530

    first commit

--- provider.tf
+++ provider.tf
@@ -11,5 +11,10 @@ terraform {
 provider "newrelic" {
   # account_id = 3660048  # Your New Relic accNRAK-KAA0NS59T13Eount ID
   # api_key = "NRAK-FQQ0TCAMOE76P7WCM6CUGR6BQ39" # Your New Relic user key
+  # account_id = 3660048  # Your New Relic accNRAK-KAA0NS59T13Eount ID
+  # api_key = "NRAK-FQQ0TCAMOE76P7WCM6CUGR6BQ39" # Your New Relic user key
+
+  # api_key : ${{ secrets.NEW_RELIC_API_KEY }}
+  # account_id: ${{ secrets.NEW_RELIC_ACCOUNT_ID }}  
   region  = "US"        # US or EU (defaults to US)
 }
