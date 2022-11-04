ec18e491fbb6c6a8ec8eab13d037cbabc0fd4a70
ihappysoni <96763614+ihappysoni@users.noreply.github.com>
2022-11-04 20:39:07 +0530

    next commit

--- provider.tf
+++ provider.tf
@@ -14,7 +14,7 @@ provider "newrelic" {
   # account_id = 3660048  # Your New Relic accNRAK-KAA0NS59T13Eount ID
   # api_key = "NRAK-FQQ0TCAMOE76P7WCM6CUGR6BQ39" # Your New Relic user key
 
-  # api_key : ${{ secrets.NEW_RELIC_API_KEY }}
-  # account_id: ${{ secrets.NEW_RELIC_ACCOUNT_ID }}  
+  api_key : var.api_key 
+  account_id: var.account_id
   region  = "US"        # US or EU (defaults to US)
 }
