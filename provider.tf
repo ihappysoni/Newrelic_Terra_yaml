54e509eb827160b11b0b04f3509d6139b393bf2f
ihappysoni <96763614+ihappysoni@users.noreply.github.com>
2022-11-04 20:43:01 +0530

    next 2 commit

--- provider.tf
+++ provider.tf
@@ -14,7 +14,7 @@ provider "newrelic" {
   # account_id = 3660048  # Your New Relic accNRAK-KAA0NS59T13Eount ID
   # api_key = "NRAK-FQQ0TCAMOE76P7WCM6CUGR6BQ39" # Your New Relic user key
 
-  api_key : var.api_key 
-  account_id: var.account_id
+  account_id =  var.account_id
+  api_key = var.api_key
   region  = "US"        # US or EU (defaults to US)
 }
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "api_key" {
+  default = api_key
+}
+variable "account_id" {
+  default = account_id
+}
